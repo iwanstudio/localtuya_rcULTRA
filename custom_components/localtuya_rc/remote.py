68a7d34f17aff782321d73a77caa61798a1e1d19
iwanstudio <98710320+iwanstudio@users.noreply.github.com>
2026-03-22 09:02:32 +0100

    Update remote.py

--- custom_components/localtuya_rc/remote.py
+++ custom_components/localtuya_rc/remote.py
@@ -2,17 +2,16 @@ import logging
 import asyncio
 import async_timeout
 from homeassistant.components.remote import RemoteEntity
-from homeassistant.const import CONF_NAME, CONF_HOST, CONF_DEVICE_ID, CONF_ACCESS_TOKEN
+from .__init__ import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-
-# Ustawiamy timeout na 3 sekundy, żeby HA nie wisiał w nieskończoność
-CONNECTION_TIMEOUT = 3.0
+CONNECTION_TIMEOUT = 3.0 # Maksymalny czas czekania na pilota
 
 class TuyaRemote(RemoteEntity):
-    def __init__(self, device, name):
+    def __init__(self, device, name, entry_id):
         self._device = device
         self._name = name
+        self._attr_unique_id = f"{entry_id}_remote"
         self._available = True
         self._state = False
 
@@ -21,13 +20,12 @@ class TuyaRemote(RemoteEntity):
         return self._name
 
     @property
-    def is_on(self):
-        return self._state
+    def available(self) -> bool:
+        return self._available
 
     @property
-    def available(self) -> bool:
-        """Zwraca dostępność urządzenia. Jeśli False, HA wyszarzy encję."""
-        return self._available
+    def is_on(self):
+        return self._state
 
     async def async_turn_on(self, **kwargs):
         self._state = True
@@ -38,35 +36,32 @@ class TuyaRemote(RemoteEntity):
         self.async_write_ha_state()
 
     async def async_update(self):
-        """Cykliczne sprawdzanie statusu urządzenia (Health Check)."""
+        """Sprawdzanie czy pilot żyje bez mrożenia HA."""
         try:
-            # Używamy executor_job, by operacja sieciowa nie blokowała pętli głównej
             async with async_timeout.timeout(CONNECTION_TIMEOUT):
+                # Wykonujemy status w osobnym wątku
                 await self.hass.async_add_executor_job(self._device.status)
                 self._available = True
-        except (asyncio.TimeoutError, Exception):
+        except Exception:
             if self._available:
-                _LOGGER.warning("Kontroler Tuya IR '%s' przestał odpowiadać (Timeout).", self._name)
+                _LOGGER.warning("Pilot ULTRA '%s' nie odpowiada - przechodzę w tryb offline.", self._name)
             self._available = False
 
     async def async_send_command(self, command, **kwargs):
-        """Wysyłanie kodów IR z zabezpieczeniem przed zamarznięciem."""
+        """Bezpieczne wysyłanie komend IR."""
         if not self._available:
-            _LOGGER.error("Nie można wysłać komendy do '%s' - urządzenie jest OFFLINE.", self._name)
+            _LOGGER.error("Pilot '%s' jest offline. Komenda zignorowana.", self._name)
             return
 
         for cmd in command:
             try:
-                _LOGGER.debug("Wysyłanie kodu IR do %s: %s", self._name, cmd)
-                # Kluczowe: async_add_executor_job zapobiega zamrożeniu interfejsu
                 async with async_timeout.timeout(CONNECTION_TIMEOUT):
                     await self.hass.async_add_executor_job(self._device.send_command, cmd)
             except asyncio.TimeoutError:
-                _LOGGER.error("Timeout przy wysyłaniu komendy do '%s'. Oznaczam jako niedostępne.", self._name)
+                _LOGGER.error("Timeout podczas wysyłania IR do '%s'. System HA działa dalej.", self._name)
                 self._available = False
                 break
             except Exception as e:
-                _LOGGER.error("Błąd wysyłania komendy do '%s': %s", self._name, e)
+                _LOGGER.error("Błąd krytyczny pilota '%s': %s", self._name, e)
                 break
-        
         self.async_write_ha_state()
